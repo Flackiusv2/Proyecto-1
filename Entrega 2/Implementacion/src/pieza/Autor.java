c38d2da25ff0d9d86cc614b5c4fa303a6fde0acb
felpg <felpg@192.168.0.105>
2024-04-15 21:39:32 -0500

    Esqueleto de clases para implementar

--- Entrega 2/Implementacion/src/pieza/Autor.java
+++ Entrega 2/Implementacion/src/pieza/Autor.java
@@ -1,5 +1,27 @@
 package pieza;
 
-public class Autor {
+import java.util.List;
 
+public class Autor {
+	
+	private String nombre;
+	private  List<Pieza> piezas;
+	
+	public Autor(String nombre, List<Pieza> piezas) {
+		this.nombre = nombre;
+		this.piezas = piezas;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	public List<Pieza> getPiezas() {
+		return piezas;
+	}
+	//implementar 
+	//public getPieza(Pieza pieza) {}
+		
+	
+	
+	
 }
--- Entrega 2/Implementacion/src/pieza/Pieza.java
+++ Entrega 2/Implementacion/src/pieza/Pieza.java
@@ -2,4 +2,13 @@ package pieza;
 
 public class Pieza {
 
+	private String titulo;
+	private String año;
+	private int valor;
+	private boolean disponible;
+	
+	
+	
+	
+	
 }
