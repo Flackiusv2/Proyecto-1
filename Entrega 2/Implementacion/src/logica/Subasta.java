c33f36878df0a2a5bfbca2a439159a5187daa6c4
felpg <felpg@192.168.0.102>
2024-04-17 16:10:19 -0500

    cositas

--- Entrega 2/Implementacion/src/logica/Subasta.java
+++ Entrega 2/Implementacion/src/logica/Subasta.java
@@ -1,5 +1,19 @@
 package logica;
 
-public class Subasta {
+import java.util.List;
 
+import pieza.Pieza;
+
+public class Subasta {
+	
+	private String nombre;
+	private boolean abierta;
+	private List<Pieza> piezasDisponibles;
+	
+	public Subasta(boolean abierta, List<Pieza> piezasDisponibles) {
+		this.nombre = nombre;
+		this.abierta = abierta;
+		this.piezasDisponibles = piezasDisponibles;
+	}
+	
 }
--- Entrega 2/Implementacion/src/usuario/Usuario.java
+++ Entrega 2/Implementacion/src/usuario/Usuario.java
@@ -2,4 +2,21 @@ package usuario;
 
 public class Usuario {
 
+	private String nombre;
+	private String contraseña;
+	
+	public Usuario(String nombre, String contraseña) {
+		this.nombre = nombre;
+		this.contraseña = contraseña;
+	}
+	
+	public boolean login(String Lnombre, String Lcontraseña) {
+		if (Lnombre == nombre){
+			if (Lcontraseña == contraseña){
+				return true;
+			}
+		}
+		return false;
+		
+	} 
 }
