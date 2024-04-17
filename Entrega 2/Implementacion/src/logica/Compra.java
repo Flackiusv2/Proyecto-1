ceeb1eef234b49e3af5e1c0d23293047681975ae
xxiongyu <x.chen@uniandes.edu.co>
2024-04-17 16:22:04 -0500

    cracion clase compra

--- Entrega 2/Implementacion/src/logica/Compra.java
+++ Entrega 2/Implementacion/src/logica/Compra.java
@@ -1,5 +1,20 @@
 package logica;
 
 public class Compra {
+	
+	private String medio;
+	private String valorPagado;
+	
+	public Compra (String medioDePago, String valor) {
+		
+		this.medio = medioDePago;
+		this.valorPagado = valor;
+	}
+	public String getMedioDePago() {
+		return medio;
+	}
+	public String getValorPagado() {
+		return valorPagado;
+	}
 
 }
