98550c2f0ef76eda512732128b3f8a4ea74cac60
felpg <felpg@192.168.0.102>
2024-04-17 14:44:47 -0500

    Pieza full

--- Entrega 2/Implementacion/src/pieza/Pieza.java
+++ Entrega 2/Implementacion/src/pieza/Pieza.java
@@ -4,9 +4,40 @@ public class Pieza {
 
 	private String titulo;
 	private String año;
-	private int valor;
+	private String lugarCreacion;
 	private boolean disponible;
-	
+	private int valor;
+	private Autor autor;
+	public Pieza(String titulo, String año, String lugarCreacion, boolean disponible, int valor, Autor autor) {
+		super();
+		this.titulo = titulo;
+		this.año = año;
+		this.lugarCreacion = lugarCreacion;
+		this.disponible = disponible;
+		this.valor = valor;
+		this.autor = autor;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getAño() {
+		return año;
+	}
+	public String getLugar() {
+		return lugarCreacion;
+	}
+	private boolean estaDisponible() {
+		return disponible;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	private void bloquear() {
+		 disponible = false;
+	}
 	
 	
 	
