8dadfedf00270a6f884b8804bbaab8c8107a1f23
xxiongyu <x.chen@uniandes.edu.co>
2024-04-19 21:01:45 -0500

    cliente ext usuario

--- Entrega 2/Implementacion/src/usuario/Cliente.java
+++ Entrega 2/Implementacion/src/usuario/Cliente.java
@@ -1,11 +1,79 @@
 package usuario;
 
-public class Cliente extends Usuario{
+import java.util.HashMap;
 
-	public Cliente(String nombre, String contraseña) {
-		super(nombre, contraseña);
-		// TODO Auto-generated constructor stub
+import logica.Compra;
+import logica.compraController;
+import pieza.Pieza;
+
+public class Cliente extends Usuario{
+	private String nombre;
+	private String telefono;
+	private String correo;
+	private HashMap<String, Compra> historialCompras;
+	private HashMap<String, Pieza> piezasPosesion;
+	private int valorMax;
+	
+	public Cliente(String cnombre, String contraseña, String ctelefono, String ccorreo) {
+		super(cnombre, contraseña);
+		telefono = ctelefono;
+		correo = ccorreo;
 	}
+	
+	public void setValorMax(int valor) {
+		this.valorMax = valor;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public String getTelefono(){
+		return this.telefono;	
+	}
+	public String getCorreo() {
+		return this.correo;
+	}
+	
+	public HashMap<String, Compra> getHistoralCompras(){
+		return this.historialCompras;
+		
+	}
+
+	public void comprar(String medio, String titulo, HashMap<String, Pieza>exhi, HashMap<String, Pieza>bodega) {
+		Pieza pieza = exhi.get(titulo);
+		if (pieza == null) {
+			pieza = bodega.get(titulo);
+			if (pieza == null) {
+				System.out.println("no existe la pieza");
+			}
+		}
+		
+		int valor = pieza.getValor();
+		compraController.hacerCompra(medio, valor);
+		
+	}
+		
+	public void devolucion(String titulo, boolean confirmacion, HashMap<String, Pieza>exhi, HashMap<String, Pieza>bodega) {
+		Pieza pieza = exhi.get(titulo);
+		if (pieza == null) {
+			pieza = bodega.get(titulo);
+			if (pieza == null) {
+				System.out.println("no existe la pieza");
+			}
+		}
+		
+		if (pieza== piezasPosesion.get(titulo)){
+			piezasPosesion.remove(titulo);
+			historialCompras.remove(titulo);
+		}
+		
+	}
+	
+	
+	
+}
+
+
 
 	
 	
