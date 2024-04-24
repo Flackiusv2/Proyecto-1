6c103ea7431ba9d7e65702452f49a77c57305e71
felpg <felpg@192.168.0.105>
2024-04-23 22:05:31 -0500

    usuario fixed

--- Entrega 2/Implementacion/src/usuario/Cliente.java
+++ Entrega 2/Implementacion/src/usuario/Cliente.java
@@ -1,98 +1,35 @@
 package usuario;
 
 
-<<<<<<< HEAD
 
-public class Cliente extends Usuario{
-=======
-public class Cliente extends Usuario{
-	
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-	private String nombre;
-	private String telefono;
-	private String id;
-	
-<<<<<<< HEAD
-	
-	public Cliente(String nombre, String contraseña, String cnombre, String ctelefono, String idd) {
-=======
-<<<<<<< HEAD
-	public Cliente(String cnombre, String contraseña, String ctelefono, String ccorreo) {
-		super(cnombre, contraseña);
-=======
-	public Cliente(String nombre, String contraseña, String cnombre, String ctelefono, String ccorreo) {
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-		super(nombre, contraseña);
-		nombre = cnombre;
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-		telefono = ctelefono;
-<<<<<<< HEAD
-		id = idd;
-=======
-		correo = ccorreo;
-<<<<<<< HEAD
+public class Cliente extends Usuario {
+    private String nombre;
+    private String telefono;
+    private String id;
 
-=======
-		historialCompras = new HashMap<String, Compra>();
-		piezasPosesion = new HashMap<String, Pieza>();
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-	}
+    public Cliente(String login, String password, String nombre, String telefono, String id) {
+        super(login, password);
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.id = id;
+    }
 
-	
-	public String getNombre() {
-		return this.nombre;
-	}
-	public String getTelefono(){
-		return this.telefono;	
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-<<<<<<< HEAD
+    public String getTelefono() {
+        return telefono;
+    }
 
-	public String getId() {
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getId() {
         return id;
     }
-=======
-<<<<<<< HEAD
-	
-
-
-
-	public void comprar(String medio, String titulo, HashMap<String, Pieza>exhi, HashMap<String, Pieza>bodega) {
-=======
-	public void comprar(String medio, String titulo, String almacenamiento, Administrador admin,HashMap<String, Pieza>exhi, HashMap<String, Pieza>bodega) {
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-		Pieza pieza = exhi.get(titulo);
-		if (pieza == null) {
-			pieza = bodega.get(titulo);
-			if (pieza == null) {
-				System.out.println("no existe la pieza");
-			}
-		}
-		
-		int valor = pieza.getValor();
-		Cajero cajeroCliente = new Cajero();
-		cajeroCliente.registrarPago(medio, valor, titulo, almacenamiento, this, admin);
-		
-	}
-		
-	public void devolucion(String titulo, boolean confirmacion, HashMap<String, Pieza>exhi, HashMap<String, Pieza>bodega) {
-		Pieza pieza = exhi.get(titulo);
-		if (pieza == null) {
-			pieza = bodega.get(titulo);
-			if (pieza == null) {
-				System.out.println("no existe la pieza");
-			}
-		}
-		
-		if (pieza== piezasPosesion.get(titulo)){
-			piezasPosesion.remove(titulo);
-			historialCompras.remove(titulo);
-		}
-		
-	}
-	
-	
->>>>>>> branch 'main' of https://github.com/Flackiusv2/Proyecto-1.git
-	
 }
